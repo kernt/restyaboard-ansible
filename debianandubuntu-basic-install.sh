57316b81d66b360c69203c78905d510fa8eb85c3
Justin B <abjustinbowen@yahoo.com>
2017-04-26 19:12:52 -0700

    Basic local install script
    install ansible and then install playbook.

---
+++ debianandubuntu-basic-install.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+# If you are running a debian variant with apt-get this should work for local install... we do not support this script, however.
+
+sudo apt-get install python-pip ansible -y
+sudo ansible-playbook restyaboard-ansible.yml
