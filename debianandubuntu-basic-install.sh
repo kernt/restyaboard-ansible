187e669cec73f5112fa2dfea7a5a3dd5e07c77d9
Justin B <abjustinbowen@yahoo.com>
2017-04-26 19:13:12 -0700

    Update debianandubuntu-basic-install.sh

--- debianandubuntu-basic-install.sh
+++ debianandubuntu-basic-install.sh
@@ -2,4 +2,4 @@
 # If you are running a debian variant with apt-get this should work for local install... we do not support this script, however.
 
 sudo apt-get install python-pip ansible -y
-sudo ansible-playbook restyaboard-ansible.yml
+sudo ansible-playbook restyaboard.yml
