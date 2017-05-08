b535073620a07427be00ea68ab849beda4177149
Justin B <abjustinbowen@yahoo.com>
2017-05-08 16:31:06 -0700

    Update debianandubuntu-basic-install.sh

--- debianandubuntu-basic-install.sh
+++ debianandubuntu-basic-install.sh
@@ -2,8 +2,10 @@
 # If you are running a debian variant with apt-get this should work for local install... we do not support this script, however.
 if [ "$1" = '-r' ] ; then 
   echo "restarting from last state."
+  sudo -u $USER git pull
   sudo ansible-playbook restyaboard.yml -c local  --limit @/home/$USER/restyaboard.retry
 else
+  sudo -u $USER git pull
   sudo apt-get install python-pip python-dev libssl-dev build-essential libtool autoconf -y
   sudo pip install --upgrade pip
   sudo pip install ansible
