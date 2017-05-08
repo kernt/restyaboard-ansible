9ac75b94ccba7bc1bad0519ef2eca0b4343f2708
Justin B <abjustinbowen@yahoo.com>
2017-05-08 13:41:07 -0700

    Update debianandubuntu-basic-install.sh

--- debianandubuntu-basic-install.sh
+++ debianandubuntu-basic-install.sh
@@ -4,6 +4,5 @@
 sudo apt-get install python-pip python-dev libssl-dev build-essential libtool autoconf -y
 sudo pip install --upgrade pip
 sudo pip install ansible
-echo "[local]" >> sudo tee /etc/ansible/hosts
-echo "localhost" >> sudo tee /etc/ansible/hosts 
+sudo cp -f ./defaults/hosts /etc/ansible/hosts 
 sudo ansible-playbook restyaboard.yml -c local
