71e99737d18c98187a2f2541527ec5fa36546563
Justin B <abjustinbowen@yahoo.com>
2017-05-08 14:49:38 -0700

    Update debianandubuntu-basic-install.sh
    Add retry option to bash script

--- debianandubuntu-basic-install.sh
+++ debianandubuntu-basic-install.sh
@@ -1,9 +1,13 @@
 #! /bin/bash
 # If you are running a debian variant with apt-get this should work for local install... we do not support this script, however.
+if [ $1 == "-r"]; then 
+  sudo ansible-playbook restyaboard.yml -c local  --limit @/home/shades/restyab$
+else
+  sudo apt-get install python-pip python-dev libssl-dev build-essential libtool autoconf -y
+  sudo pip install --upgrade pip
+  sudo pip install ansible
+  sudo mkdir -p /etc/ansible
+  sudo cp -f ./defaults/hosts /etc/ansible/hosts 
+  sudo ansible-playbook restyaboard.yml -c local 
+fi
 
-sudo apt-get install python-pip python-dev libssl-dev build-essential libtool autoconf -y
-sudo pip install --upgrade pip
-sudo pip install ansible
-sudo mkdir -p /etc/ansible
-sudo cp -f ./defaults/hosts /etc/ansible/hosts 
-sudo ansible-playbook restyaboard.yml -c local
